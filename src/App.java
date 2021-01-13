7faa9d3990b452ddc7146d0068b625dd71f16a9b
Everton Dutra <evertondutra02@gmail.com>
2021-01-13 02:42:05 -0300

    criando uma pagina pessoal

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        
+        System.out.println("Hello, Todos!" + numero);
+    }
+
+}
