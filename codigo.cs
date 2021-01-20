0d77133fd36091ab8bc404548c23f35b938bc09c
Eduardo <eduardof.dutra@yahoo.com>
2021-01-20 11:29:37 -0300

    qualquer coisa

---
+++ codigo.cs
@@ -0,0 +1,11 @@
+int horasdeViagem = 10
+int velocidadeMedia = 85
+int kilometroPorL = 12
+int distPercorrida
+int litrosViagem
+
+distPercorrida = horasdeViagem * velocidadeMedia
+
+litrosViagem = kilometroPorL / distPercorrida
+
+
